1143fe764e88c6947d19d8dc622f718165f3f996
Ed <e.k.dev@outlook.com>
2015-10-11 07:58:03 +0200

    created choicelist component

--- client/views/components/ChoiceItem.jsx
+++ client/views/components/ChoiceItem.jsx
@@ -1,4 +1,4 @@
-PollItem = React.createClass({
+ChoiceItem = React.createClass({
   render() {
     var id = Random.id();
     var divStyle = {
@@ -6,13 +6,17 @@ PollItem = React.createClass({
     };
     return (
       <li className="collection-item dismissable">
+
           <div className="row">
+
             <div className="input-field col s11">
               <FormField type="text" name="choice" />
             </div>
+
             <a className="secondary-content btn-floating btn waves-effect waves-light red">
               <i className="zmdi zmdi-delete zmdi-hc-2x"></i>
             </a>
+
           </div>
 
       </li>
